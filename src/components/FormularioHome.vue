803f956465427a154ae0af986831a9d8ab7aadd4
André Campelo <decocampeloreis@gmail.com>
2023-02-08 22:13:50 -0300

    finalizando função do contador

--- src/components/FormularioHome.vue
+++ src/components/FormularioHome.vue
@@ -41,7 +41,8 @@ export default defineComponent({
   name: "FormularioHome",
   data() {
     return{
-        tempoEmSegundos: 0
+        tempoEmSegundos: 0,
+        cronometro: 0
     }
   },
   computed: {
@@ -51,13 +52,12 @@ export default defineComponent({
   },
   methods: {
     iniciar() {
-        setInterval(() =>{
+       this.cronometro = setInterval(() => {
             this.tempoEmSegundos += 1
         }, 1000)
-        console.log('iniciando')
     },
     finalizar() {
-        console.log('finalizando')
+        clearInterval(this.cronometro)
     }
   }
 });
