1456e1612618bb725b2472c069eae11dd1a971f0
André Campelo <decocampeloreis@gmail.com>
2023-02-08 23:31:25 -0300

    criando componente de Temporizador

--- src/components/FormularioHome.vue
+++ src/components/FormularioHome.vue
@@ -14,19 +14,7 @@
       </div>
 
       <div class="column">
-        <div
-          class="is-flex is-align-items-center is-justify-content-space-between"
-        >
-          <CronometroFormulario :tempoEmSegundos="tempoEmSegundos"/>
-          <button class="button" @click="iniciar()">
-            <span class="material-symbols-outlined"> play_arrow </span>
-            <span>play</span>
-          </button>
-          <button class="button" @click="finalizar">
-            <span class="material-symbols-outlined"> stop </span>
-            <span>stop</span>
-          </button>
-        </div>
+        <TemporizadorFormulario/>
       </div>
     </div>
   </div>
@@ -34,7 +22,7 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import CronometroFormulario from "./CronometroFormulario.vue";
+import TemporizadorFormulario from "./TemporizadorFormulario.vue";
 
 export default defineComponent({
   name: "FormularioHome",
@@ -45,17 +33,7 @@ export default defineComponent({
     }
   },
   components: {
-    CronometroFormulario
-  },
-  methods: {
-    iniciar() {
-       this.cronometro = setInterval(() => {
-            this.tempoEmSegundos += 1
-        }, 1000)
-    },
-    finalizar() {
-        clearInterval(this.cronometro)
-    }
+    TemporizadorFormulario
   }
 });
 </script>
---
+++ src/components/TemporizadorFormulario.vue
@@ -0,0 +1,43 @@
+<template>
+    <div
+    class="is-flex is-align-items-center is-justify-content-space-between"
+  >
+    <CronometroFormulario :tempoEmSegundos="tempoEmSegundos"/>
+    <button class="button" @click="iniciar">
+      <span class="material-symbols-outlined"> play_arrow </span>
+      <span>play</span>
+    </button>
+    <button class="button" @click="finalizar">
+      <span class="material-symbols-outlined"> stop </span>
+      <span>stop</span>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import CronometroFormulario from "./CronometroFormulario.vue";
+
+export default defineComponent({
+  name: "TemporizadorFormulario",
+  data() {
+    return{
+        tempoEmSegundos: 0,
+        cronometro: 0
+    }
+  },
+  components: {
+    CronometroFormulario
+  },
+  methods: {
+    iniciar() {
+       this.cronometro = setInterval(() => {
+            this.tempoEmSegundos += 1
+        }, 1000)
+    },
+    finalizar() {
+        clearInterval(this.cronometro)
+    }
+  }
+});
+</script>
