8275f704e9f7629866b21f50ce471c5705f4d371
André Campelo <decocampeloreis@gmail.com>
2023-02-08 21:38:07 -0300

    criando componente formulário

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,27 @@
 <template>
   <main class="columns is-gapless is-multiline">
       <div class="column is-one-quarter">
-         ???
+         <BarraLateral/>
       </div>
 
       <div class="column is-three-quarter">
-         ???
+         <FormularioHome/>
       </div>
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue';
+import FormularioHome from './components/FormularioHome.vue';
 
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  components: { 
+    BarraLateral,
+    FormularioHome
+  }
 
 });
 </script>
---
+++ src/components/FormularioHome.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="Formulário para criaçäo de uma nova tarefa"
+      >
+        <input
+          type="text "
+          class="input"
+          placeholder="Qual tarefa você deseja inciar ?"
+        />
+      </div>
+
+      <div class="column">
+        <div
+          class="is-flex is-align-items-center is-justify-content-space-between"
+        >
+          <section>
+            <strong>00:00:00</strong>
+          </section>
+          <button class="button">
+            <span class="material-symbols-outlined"> play_arrow </span>
+            <span>play</span>
+          </button>
+          <button class="button">
+            <span class="material-symbols-outlined"> stop </span>
+            <span>stop</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "FormularioHome",
+});
+</script>
+
+<style scoped>
+</style>
